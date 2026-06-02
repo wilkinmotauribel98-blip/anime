db4ec94af56fd90ae81f652eff47401ece8525d1
spider <wilkinmotauribel98@gmail.com>
2026-06-02 19:00:58 -0400

    act

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "https://esm.sh/react"
 import { createRoot } from 'https://esm.sh/react-dom/client'
-import { App } from './App'
+import { App } from './App.jsx'
 import "./App.css"
 
 createRoot(document.getElementById('root')).render(
